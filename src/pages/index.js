e825182f76dc2a6a5d435a1d113acc91eab0eb37
kishore007k <kishorekumarstar07@gmail.com>
2020-10-05 21:55:44 +0530

    Added: Styles
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from "react";
+import {Container} from "../components/Container";
 
-export default function Home() {
-  return <div>Hello world!</div>
+const  Home = () => {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  )
 }
+
+export default Home;
