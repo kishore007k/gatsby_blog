115019d3aebaac39fcd3444f66c1a494b18dbf9c
kishore007k <kishorekumarstar07@gmail.com>
2020-10-05 23:01:58 +0530

    Added: Footer
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,74 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  FooterWrapper,
+  FooterSocialIcons,
+  FooterSocialWrapper,
+} from "../elements"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      codepen: file(relativePath: { eq: "codepen.svg" }) {
+        publicURL
+      }
+      github: file(relativePath: { eq: "github.svg" }) {
+        publicURL
+      }
+      instagram: file(relativePath: { eq: "instagram.svg" }) {
+        publicURL
+      }
+      linkedIn: file(relativePath: { eq: "linkedin.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <FooterWrapper>
+      <FooterSocialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="facebook logo" />
+          </a>
+          <a
+            href="https://codepen.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.codepen.publicURL} alt="codepen logo" />
+          </a>
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.github.publicURL} alt="github logo" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.instagram.publicURL} alt="instagram logo" />
+          </a>
+          <a
+            href="https://linkedIn.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.linkedIn.publicURL} alt="linkedIn logo" />
+          </a>
+        </FooterSocialIcons>
+        <p>Copyright © Company. All rights reserved.</p>
+      </FooterSocialWrapper>
+    </FooterWrapper>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import {Container} from "../components/Container";
 const  Home = () => {
   return (
     <Container>
-      <h1>Hello World!</h1>
+      
     </Container>
   )
 }
