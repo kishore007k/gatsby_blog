0ae17da2ccde73c3a2cdf049e4a58beeecc2ac95
kishore007k <kishorekumarstar07@gmail.com>
2020-10-05 21:17:57 +0530

    Added: global styles
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,25 @@
-import React from "react"
-import { ThemeProvider } from "styled-components"
-import Theme from "./src/themes/theme"
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Theme from "./src/themes/theme";
+
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body, html{
+    font-family: ${props => props.theme.fonts.main};
+    height: 100%;
+    background-color: ${props => props.theme.colors.light1};
+  }
+`;
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
 )
